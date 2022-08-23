1b7b4223639675e888a7907a17dce96af96bd8bd
Shaharrechtman <110894284+Shaharrechtman@users.noreply.github.com>
2022-08-23 17:24:48 +0300

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# PRINT LOVE
+
+# # CELL 1 
+
+# # Shahar  
+# Marketing analyst
+
+# I am interested in data science because I believe it is a part of every single thing in life and is able to help in every single field 
+
+# ### simple math
+
+# In[1]:
+
+
+1+1
+
+
+# |header|header|
+# |----|----|
+# |----|----|
+# |----|----|
+# |cell|cell|
+
+# |header|header|
+# |---|----|
+# |one|one|
+*one
+*two
+*three
+# In[ ]:
+
+
+
+
